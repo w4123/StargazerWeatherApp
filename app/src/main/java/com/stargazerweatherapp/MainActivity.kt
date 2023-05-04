8311b6fb3127704af42725087629047e6970ca5d
w4123 <1840686745@qq.com>
2023-05-04 17:12:25 +0100

    Add Preview

--- app/src/main/java/com/stargazerweatherapp/MainActivity.kt
+++ app/src/main/java/com/stargazerweatherapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -13,33 +15,38 @@ import com.stargazerweatherapp.ui.screens.MainScreen
 import com.stargazerweatherapp.ui.theme.StargazerWeatherAppTheme
 
 class MainActivity : ComponentActivity() {
+    @Preview
+    @Composable
+    fun MainScreen() {
+        StargazerWeatherAppTheme {
+            Surface {
+                val navController = rememberNavController()
+
+                NavHost(navController, startDestination = "main") {
+                    composable("main") {
+                        MainScreen(
+                            navigateToDetailsScreen = { navController.navigate("details") },
+                            navigateToSettingsScreen = { navController.navigate("settings") }
+                        )
+                    }
+                    composable("details") {
+                        DetailsScreen(
+                            onBackClick = { navController.popBackStack() }
+                        )
+                    }
+                    //composable("settings") {
+                    //    SettingsScreen(
+                    //        onBackClick = { navController.popBackStack() }
+                    //    )
+                    //}
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StargazerWeatherAppTheme {
-                Surface {
-                    val navController = rememberNavController()
-
-                    NavHost(navController, startDestination = "main") {
-                        composable("main") {
-                            MainScreen(
-                                navigateToDetailsScreen = { navController.navigate("details") },
-                                navigateToSettingsScreen = { navController.navigate("settings") }
-                            )
-                        }
-                        composable("details") {
-                            DetailsScreen(
-                                onBackClick = { navController.popBackStack() }
-                            )
-                        }
-                        //composable("settings") {
-                        //    SettingsScreen(
-                        //        onBackClick = { navController.popBackStack() }
-                        //    )
-                        //}
-                    }
-                }
-            }
+            MainScreen()
         }
     }
 }
--- app/src/main/java/com/stargazerweatherapp/ui/screen/MainScreen.kt
+++ app/src/main/java/com/stargazerweatherapp/ui/screen/MainScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 //import com.stargazerweatherapp.ui.components.AstronomyCard
