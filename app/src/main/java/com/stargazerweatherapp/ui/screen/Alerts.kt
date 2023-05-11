a45a9731dbc245f3f53360f713927422949a49a7
archonus <excalibur.archonus@gmail.com>
2023-05-11 14:27:50 +0100

    Added AlertPage

---
+++ app/src/main/java/com/stargazerweatherapp/ui/screen/Alerts.kt
@@ -0,0 +1,11 @@
+package com.stargazerweatherapp.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun AlertPage()
+{
+
+}
