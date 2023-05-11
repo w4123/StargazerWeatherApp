853ce75a6ed58b2b2eeff7de5588fb6a4d70eace
archonus <excalibur.archonus@gmail.com>
2023-05-11 14:48:37 +0100

    Changed the theme

--- app/src/main/java/com/stargazerweatherapp/ui/theme/Theme.kt
+++ app/src/main/java/com/stargazerweatherapp/ui/theme/Theme.kt
@@ -7,8 +7,8 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
+    primary = Purple2,
+    primaryVariant = Purple1,
     secondary = Teal200
 )
 
