4300520aa8ca033d0f04234c26c645ee1abca0bc
Lucy <lh785@cam.ac.uk>
2023-05-16 16:03:37 +0100

    Calendar on click working, background colours modified

--- app/src/main/java/com/stargazerweatherapp/ui/screen/Calendar.kt
+++ app/src/main/java/com/stargazerweatherapp/ui/screen/Calendar.kt
@@ -1,34 +1,35 @@
 package com.stargazerweatherapp.ui.screen
 
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import io.github.boguszpawlowski.composecalendar.*
-import io.github.boguszpawlowski.composecalendar.header.MonthState
+import androidx.navigation.compose.rememberNavController
+import com.stargazerweatherapp.ui.theme.Purple200
+import com.stargazerweatherapp.ui.theme.Purple500
+import io.github.boguszpawlowski.composecalendar.SelectableCalendar
+import io.github.boguszpawlowski.composecalendar.rememberSelectableCalendarState
 
 @Composable
 @Preview
 fun CalendarA() {
     val calendarState = rememberSelectableCalendarState()
-    SelectableCalendar(
-        calendarState = calendarState,
-    )
+    val navController = rememberNavController()
+    Column(Modifier.background(color = Purple500).fillMaxHeight()) {
 
-    Button(onClick = { println(calendarState.selectionState.selection) }, modifier = Modifier.padding(
-        Dp(100F)
-    )) {
+        SelectableCalendar(
+            calendarState = calendarState,
+            modifier = Modifier.background(color = Purple200)
+        )
 
+        if (calendarState.selectionState.selection.isNotEmpty()){
+            // navController.navigate("Alerts")
+            println("Went to " + calendarState.selectionState.selection.toString())
+        }
     }
 
-}
-
-fun onClick(){
-    println("goToAThing")
 }
