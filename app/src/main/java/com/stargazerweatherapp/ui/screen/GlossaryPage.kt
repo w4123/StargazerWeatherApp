5997e1e399a5bbebb9f346b12637dce6add2f53f
archonus <excalibur.archonus@gmail.com>
2023-05-18 17:29:02 +0100

    Added text to glossary page

--- app/src/main/java/com/stargazerweatherapp/ui/screen/GlossaryPage.kt
+++ app/src/main/java/com/stargazerweatherapp/ui/screen/GlossaryPage.kt
@@ -13,7 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import org.w3c.dom.Text
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -35,13 +38,28 @@ fun Glossary(navigateBack: () -> Unit = {})
     ) {
         Column {
             Text(modifier = Modifier.padding(it), text = "Crescent Moon", fontWeight= FontWeight.Bold)
-            Text(text = "Lorem Ipsum")
+            Text(
+                buildAnnotatedString {
+                    append("Lorem ipsum dolor sit amet, consectetur adipiscing elit ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)){
+                        append("gibbon moon")
+                    }
+                    append(" euismod eleifend arcu eget placerat. Donec interdum mauris sit amet mi pharetra faucibus")
+                }
+            )
             Text(modifier = Modifier.padding(it), text = "Gibbon Moon", fontWeight= FontWeight.Bold)
-            Text(text = "Lorem Ipsum")
+            Text(buildAnnotatedString {
+                append("Aliquam quis ante porttitor, egestas turpis sed, dictum ligula ")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)){
+                    append("crescent moon")
+                }
+                append(". Proin ac lectus efficitur, fermentum turpis et, efficitur felis.")
+            })
             Text(modifier = Modifier.padding(it),text = "Seeing", fontWeight= FontWeight.Bold)
-            Text(text = "Lorem Ipsum")
+            Text(text =
+            "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer et euismod justo. Aenean et magna nec enim viverra sollicitudin.")
             Text(modifier = Modifier.padding(it),text = "Transparency", fontWeight= FontWeight.Bold)
-            Text(text = "Lorem Ipsum")
+            Text(text = "Quisque ligula urna, ultricies et tempus sollicitudin, aliquet at quam. Vivamus tempor magna id sollicitudin lobortis. Donec a odio ut ex venenatis ultricies ut eu mi.")
 
         }
 
