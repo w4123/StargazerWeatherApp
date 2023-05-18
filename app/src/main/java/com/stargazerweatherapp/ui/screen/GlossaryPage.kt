e546efed442e1ce5d3b727d414119e897d1f41ba
archonus <excalibur.archonus@gmail.com>
2023-05-18 16:24:42 +0100

    Implemented GlossaryPage

---
+++ app/src/main/java/com/stargazerweatherapp/ui/screen/GlossaryPage.kt
@@ -0,0 +1,36 @@
+package com.stargazerweatherapp.ui.screen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun Glossary(navigateBack: () -> Unit = {})
+{
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text("Set Alert")
+            },
+                navigationIcon = {
+                    IconButton(onClick = navigateBack) {
+                        Icon(Icons.Default.ArrowBack, "Back")
+                    }
+                })
+        }
+    ) {
+        Text(modifier = Modifier.padding(it), text = "Glossary Page")
+
+    }
+}
