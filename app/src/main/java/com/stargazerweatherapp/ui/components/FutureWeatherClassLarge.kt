0d8c53f4c9132db8fd8c4cf24ec7662607d25370
Lucy <lh785@cam.ac.uk>
2023-05-23 14:18:45 +0100

    Future Weather Card matches Current Weather Card

--- app/src/main/java/com/stargazerweatherapp/ui/components/FutureWeatherClassLarge.kt
+++ app/src/main/java/com/stargazerweatherapp/ui/components/FutureWeatherClassLarge.kt
@@ -1,33 +1,36 @@
 package com.stargazerweatherapp.ui.components
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.stargazerweatherapp.R
 import com.stargazerweatherapp.data.models.DailyWeather
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FutureWeatherCardLarge(weather: DailyWeather?, modifier: Modifier = Modifier, navigateToGlossary : () -> Unit) {
-    //A Large weather card for showing weather in a day in the future,
+    // A Large weather card for showing weather in a day in the future,
     // This matches the style of the weather card to create a more universal theme
+    var tooltipStateCloudCover by remember{ mutableStateOf(PlainTooltipState()) }
+    var tooltipStateVisibility by remember{ mutableStateOf(PlainTooltipState()) }
+    var tooltipStateTransparency by remember{ mutableStateOf(PlainTooltipState()) }
+    val scope = rememberCoroutineScope()
     Card(modifier = modifier.fillMaxWidth()) {
         weather?.let { nonNullWeather ->
             Column(
@@ -85,23 +88,73 @@ fun FutureWeatherCardLarge(weather: DailyWeather?, modifier: Modifier = Modifier
                 }
                 Text(
                     text = stringResource(id = nonNullWeather.weatherType.icon),
-                    fontSize = 70.sp,
+                    fontSize = 128.sp,
                     fontFamily = FontFamily(Font(R.font.weathericons))
                 )
                 Text(text = nonNullWeather.weatherType.description, fontSize = 26.sp)
                 Text(text = "${nonNullWeather.temperatureMin}°C", fontSize = 26.sp)
 
+                Spacer(Modifier.height(16.dp))
 
-                TextButton(onClick = navigateToGlossary) {
-                    Text(text = "Cloud Cover: ${nonNullWeather.cloudCoverAtSunset}%")
-                }
+                Text(text = "Cloud Cover: ${nonNullWeather.cloudCoverAtSunset}%", modifier = Modifier.clickable {
+                    scope.launch {
+                        tooltipStateCloudCover.show()
+                    }
+                })
 
-                TextButton(onClick = navigateToGlossary) {
-                    Text(text = "Visibility: ${nonNullWeather.visibility}m")
-                }
-                TextButton(onClick = navigateToGlossary) {
-                    Text(text = "Transparency: ${nonNullWeather.transparency}")
-                }
+                PlainTooltipBox(tooltip = {
+                    Column(Modifier.padding(12.dp)) {
+                        Text(text = "Cloud Cover", fontWeight= FontWeight.Bold)
+                        Text(
+                            buildAnnotatedString {
+                                append("Lorem ipsum dolor sit amet, consectetur adipiscing elit ")
+                                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)){
+                                    append("cloud cover")
+                                }
+                                append(" euismod eleifend arcu eget placerat. Donec interdum mauris sit amet mi pharetra faucibus")
+                            }
+                        )
+                    } }, tooltipState = tooltipStateCloudCover) {}
+
+                Text(text = "Visibility: ${nonNullWeather.visibility}m", modifier = Modifier.clickable {
+                    scope.launch {
+                        tooltipStateVisibility.show()
+                    }
+                })
+
+                PlainTooltipBox(tooltip = {
+                    Column(Modifier.padding(12.dp)) {
+                        Text(text = "Visibility", fontWeight= FontWeight.Bold)
+                        Text(
+                            buildAnnotatedString {
+                                append("Lorem ipsum dolor sit amet, consectetur adipiscing elit ")
+                                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)){
+                                    append("visibility")
+                                }
+                                append(" euismod eleifend arcu eget placerat. Donec interdum mauris sit amet mi pharetra faucibus")
+                            }
+                        )
+                    } }, tooltipState = tooltipStateVisibility) {}
+
+                Text(text = "Transparency: ${weather.transparency}", modifier = Modifier.clickable {
+                    scope.launch {
+                        tooltipStateTransparency.show()
+                    }
+                })
+
+                PlainTooltipBox(tooltip = {
+                    Column(Modifier.padding(12.dp)) {
+                        Text(text = "Transparency", fontWeight= FontWeight.Bold)
+                        Text(
+                            buildAnnotatedString {
+                                append("Lorem ipsum dolor sit amet, consectetur adipiscing elit ")
+                                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)){
+                                    append("cloud cover")
+                                }
+                                append(" euismod eleifend arcu eget placerat. Donec interdum mauris sit amet mi pharetra faucibus")
+                            }
+                        )
+                    } }, tooltipState = tooltipStateTransparency) {}
 
             }
         }
